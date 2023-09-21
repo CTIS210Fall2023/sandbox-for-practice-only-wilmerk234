d4d1b5232d45589d3a086c9375606614361f9f82
Wilmer Acevedo Quezada <wilmeracevedoquezada@Wilmers-MacBook-Air.local>
2023-09-21 08:40:53 -0400

    g

--- src/main/java/edu/guilford/Sandbox.java
+++ src/main/java/edu/guilford/Sandbox.java
@@ -3,6 +3,9 @@ package edu.guilford;
 public class Sandbox {
     public static void main(String[] args) {
         // A place for testing Java code
+    
+        Random rand = new Random();
+        
         
     }
 }
